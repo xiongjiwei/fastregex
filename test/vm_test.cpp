ab860fe15def7258a3532eb7ccc158b604416982
jwxiong <xiongjiwei1996@outlook.com>
2019-10-21 17:04:10 +0800

    [jwxiong] test private method.  (not good)

--- test/vm_test.cpp
+++ test/vm_test.cpp
@@ -9,36 +9,59 @@
 
 TEST_CASE("vm instructions test") {
     SECTION("character instructions") {
-        std::string matched_data = "aaa";
+        std::string matched_data = "a";
         char program[] = {0x01, 'a', 0x00};
         REx::ReVM vm = REx::ReVM(matched_data, program);
-        vm.start_vm();
-        auto matched_ret = vm.get_result();
-        auto thread_ret = vm.get_thread_info();
+        auto *thread = new REx::Thread(0, 0, 0);
+        vm.ins_character(thread);
 
-        CHECK(thread_ret.empty());
-        CHECK(matched_ret.size() == 3);
-        CHECK(matched_ret.at(0).start == 0);
-        CHECK(matched_ret.at(0).end == 1);
-        CHECK(matched_ret.at(1).start == 1);
-        CHECK(matched_ret.at(1).end == 2);
-        CHECK(matched_ret.at(2).start == 2);
-        CHECK(matched_ret.at(2).end == 3);
+        CHECK(thread->alive);
+        CHECK(thread->PC == 2);
+        CHECK(thread->SP == 1);
+    }
+
+    SECTION("character instructions") {
+        std::string matched_data = "a";
+        char program[] = {0x01, 'b', 0x00};
+        REx::ReVM vm = REx::ReVM(matched_data, program);
+        auto *thread = new REx::Thread(0, 0, 0);
+        vm.ins_character(thread);
+
+        CHECK_FALSE(thread->alive);
     }
 
     SECTION("split instructions") {
-        std::string matched_data = "ab";
-        char program[] = {0x02, 0x03, 0x06, 0x01, 'a', 0x00, 0x01, 'b', 0x00};
+        std::string matched_data = "a";
+        char program[] = {0x02, 0x03, 0x06};
         REx::ReVM vm = REx::ReVM(matched_data, program);
-        vm.start_vm();
-        auto matched_ret = vm.get_result();
-        auto thread_ret = vm.get_thread_info();
+        auto *first_thread = new REx::Thread(0, 0, 0);
+        vm.ins_split(first_thread);
+        auto second_thread = vm.running_thread_list.front();
 
-        CHECK(thread_ret.empty());
-        CHECK(matched_ret.size() == 2);
-        CHECK(matched_ret.at(0).start == 0);
-        CHECK(matched_ret.at(0).end == 1);
-        CHECK(matched_ret.at(1).start == 1);
-        CHECK(matched_ret.at(1).end == 2);
+        CHECK(first_thread->PC == 0x03);
+        CHECK(second_thread->PC == 0x06);
+    }
+
+    SECTION("jmp instructions") {
+        std::string matched_data = "a";
+        char program[] = {0x03, 0x03, 0x06};
+        REx::ReVM vm = REx::ReVM(matched_data, program);
+        auto *thread = new REx::Thread(0, 0, 0);
+        vm.ins_jmp(thread);
+
+        CHECK(thread->PC == 0x03);
+    }
+
+    SECTION("match instructions") {
+        std::string matched_data = "aaaaa";
+        char program[] = {0x00};
+        REx::ReVM vm = REx::ReVM(matched_data, program);
+        auto *thread = new REx::Thread(0, 3, 0);
+        vm.ins_match(thread);
+
+        CHECK_FALSE(thread->alive);
+        CHECK(vm.success_thread_list.size() == 1);
+        CHECK(vm.success_thread_list.at(0).start == 0);
+        CHECK(vm.success_thread_list.at(0).end == 3);
     }
 }
