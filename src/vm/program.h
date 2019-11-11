ff6da31cfcfc8a33727fbbc21d86dcb53989bd52
jwxiong <xiongjiwei1996@outlook.com>
2019-11-11 17:39:26 +0800

    [jwxiong] delete unused method

--- src/vm/program.h
+++ src/vm/program.h
@@ -28,20 +28,12 @@ namespace REx {
             this->length = _length;
         }
 
-        void set_bytecode_length(int16_t _length) {
-            this->length = _length;
-        }
-
         void set_bytecode_content(int16_t pos, BYTE byte) {
             bytecode[pos] = byte;
         }
 
         void set_bytecode_content(int16_t pos, BYTE *byte, int16_t n) {
             memcpy(bytecode + pos, byte, n);
-        }
-
-        size_t get_bytecode_length() const{
-            return this->length;
         }
 
         friend class Program;
@@ -52,6 +44,9 @@ namespace REx {
         Pro_Tree *left = nullptr;
         Pro_Tree *&child = left;
         Pro_Tree *right = nullptr;
+
+        int low = 0;
+        int high = 0;
     };
 
     class Program {
@@ -70,7 +65,7 @@ namespace REx {
         void marshal_charset(int16_t pos, Pro_Tree *pro_tree);
 
         void init_program(const Pro_Tree *pro_tree);
-        static int get_padding_byte_length(AST::NODETYPE type);
+        static int get_padding_byte_length(AST *ast);
         void marshal_int16(int16_t pos, int16_t num);
         void marshal_instruction(int16_t pos, REx::BYTE instruction);
         void marshal_content(int16_t pos, BYTE *bytecode, int16_t length);
