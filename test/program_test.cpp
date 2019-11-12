b3d5c44c098a9bbb4e8f3809b716f45a4463c0ff
jwxiong <xiongjiwei1996@outlook.com>
2019-11-12 11:12:17 +0800

    [jwxiong] write test a{0,n}

--- test/program_test.cpp
+++ test/program_test.cpp
@@ -221,4 +221,34 @@ TEST_CASE("bytecode test") {
         memcpy(t, bytecode, 25);
         CHECK(memcmp(valid_bytecode, bytecode, 25) == 0);
     }
+
+    SECTION("") {
+        std::string string = "a{0,2}b";
+        REx::REstring restring(string);
+        REx::Parser parser(restring);
+
+        REx::AST *ast = parser.exper();
+        auto bytecode = (new REx::Program())->to_bytecode(ast);
+        REx::BYTE valid_bytecode[] = {
+                0x06, 0x00, 0x00,               //loop 2
+                0x02, 0x00, 0x00, 0x00, 0x00,   //split 5, 10
+                0x01, 'a',                      //char a
+                0x07, 0x00, 0x00,               //endloop -7
+                0x01, 'b',                      //char b
+                0x00                            //match
+        };
+
+        int16_t line = 2;
+        memcpy(valid_bytecode + 1, &line, 2);
+        line = 5;
+        memcpy(valid_bytecode + 4, &line, 2);
+        line = 10;
+        memcpy(valid_bytecode + 6, &line, 2);
+        line = -7;
+        memcpy(valid_bytecode + 11, &line, 2);
+
+        REx::BYTE t[16];
+        memcpy(t, bytecode, 16);
+        CHECK(memcmp(valid_bytecode, bytecode, 16) == 0);
+    }
 }
